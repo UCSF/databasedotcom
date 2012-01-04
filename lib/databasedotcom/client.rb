1c41856e43bfd47862760c36f524830d86fbdedf
lcampos <lcamposguajardo@gmail.com>
2012-01-03 16:35:14 -0800

    out debugs

--- lib/databasedotcom/client.rb
+++ lib/databasedotcom/client.rb
@@ -162,22 +162,18 @@ module Databasedotcom
     #
     # The classes defined by materialize derive from Sobject, and have getters and setters defined for all the attributes defined by the associated Force.com Sobject.
     def materialize(classnames)
+      puts "materialize "
       classes = (classnames.is_a?(Array) ? classnames : [classnames]).collect do |clazz|
         original_classname = clazz
         clazz = original_classname[0,1].capitalize + original_classname[1..-1]
-        puts "materialize self.refresh_token = '#{self.refresh_token}' "
         unless const_defined_in_module(module_namespace, clazz)
           new_class = module_namespace.const_set(clazz, Class.new(Databasedotcom::Sobject::Sobject))
-          puts "materialize unless self.refresh_token = '#{self.refresh_token}' "
           new_class.client = self
-          puts "materialize unless self.refresh_token = '#{self.refresh_token}' "
           new_class.materialize(original_classname)
           new_class
         else
-          puts "materialize else self.refresh_token  "
           module_namespace.const_get(clazz)
         end
-        puts "materialize end self.refresh_token = '#{self.refresh_token}' "
       end
 
       classes.length == 1 ? classes.first : classes
